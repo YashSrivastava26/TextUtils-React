4e41914cbd3f9e32199ec2982187825a53885fae
YashSrivastava26 <yashsrivastava2603@gmail.com>
2023-03-25 21:15:10 +0530

    deployment on github.io

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,14 +8,14 @@ function BasicExample(props) {
   return (
     <Navbar bg={`${props.mode}`} variant={`${props.mode}`} expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Text-Utils</Navbar.Brand>
-        {/* <Navbar.Brand href="#">Text-Utils</Navbar.Brand> */}
+        {/* <Navbar.Brand as={Link} to="/">Text-Utils</Navbar.Brand> */}
+        <Navbar.Brand href="#">Text-Utils</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            {/* <Nav.Link href="#">Home</Nav.Link> */}
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            {/* <Nav.Link as={Link} to="/">Home</Nav.Link> */}
+            <Nav.Link href="#">Home</Nav.Link>
+            {/* <Nav.Link as={Link} to="/about">About</Nav.Link> */}
           </Nav>
 
           <Form.Check type="switch" onClick={props.toggleMode} label={`${props.mode} mode`}/>
